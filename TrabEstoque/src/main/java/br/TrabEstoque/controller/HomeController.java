64d1b5c16b829aeb1283361b6130ae053c428068
BrunoScholze2003 <104472351+BrunoScholze2003@users.noreply.github.com>
2023-03-24 20:21:48 -0300

    Final aula24/03

--- TrabEstoque/src/main/java/br/TrabEstoque/controller/HomeController.java
+++ TrabEstoque/src/main/java/br/TrabEstoque/controller/HomeController.java
@@ -4,15 +4,15 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping
+@RequestMapping("/")
 public class HomeController {
 
     @GetMapping
-    @ResponseBody
-    public String index(){
-        return "Eu nao acredita";
+    public ModelAndView index(){
+        return new ModelAndView("home/index");
     }
     
 }
