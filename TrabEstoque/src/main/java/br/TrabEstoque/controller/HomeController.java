1aa831d1172c9bf3b82454c083fa3cc2537e62f1
BrunoScholze2003 <104472351+BrunoScholze2003@users.noreply.github.com>
2023-03-24 19:44:37 -0300

    Add files via upload

---
+++ TrabEstoque/src/main/java/br/TrabEstoque/controller/HomeController.java
@@ -0,0 +1,18 @@
+package br.TrabEstoque.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping
+public class HomeController {
+
+    @GetMapping
+    @ResponseBody
+    public String index(){
+        return "Eu nao acredita";
+    }
+    
+}
